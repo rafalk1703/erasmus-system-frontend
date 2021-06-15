37d87ccb04f815b558cefbb8ce0cf8521b7f51cb
Rafał Krajewski <rafalk1703@gmail.com>
2021-06-15 23:22:06 +0200

    add list component

--- src/pages/Contracts.js
+++ src/pages/Contracts.js
@@ -1,49 +1,14 @@
 import React, { Component } from 'react';
-import ContractService from '../services/ContractService';
+import ContractsList from '../components/ContractsList';
 
 
 class Contracts extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            contracts: []
-        }
-    }
-
-    componentDidMount() {
-        ContractService.getAllContracts().then((response) => {
-            this.setState({ contracts: response.data.body })
-        });
-    }
-
+   
     render() {
         return (
-            < div>
-                <h1 className = 'text-center'>Contracts List</h1>
-                <table className = 'table table-striped'>
-                    <thead>
-                        <tr>
-                            <td>Contract Id</td>
-                            <td>Erasmus Code</td>
-                            <td>Vacancies</td>
-                            <td>Degree</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.contracts.map(
-                                contract => 
-                                <tr key = {contract.id}>
-                                    <td> {contract.id} </td>
-                                    <td> {contract.erasmusCode} </td>
-                                    <td> {contract.vacancies} </td>
-                                    <td> {contract.degree} </td>
-                                </tr>
-                            )
-                        }
-                    </tbody>
-                </table>
+            <div className="container">
+                <ContractsList/>
             </div>
         );
     }

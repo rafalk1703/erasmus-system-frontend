a17fa8c800923567f7ab0ff9817e4f6caa94a21b
Rafał Krajewski <rafalk1703@gmail.com>
2021-07-19 23:23:38 +0200

    delete useless code

--- src/components/UploadFile.js
+++ src/components/UploadFile.js
@@ -170,27 +170,4 @@ class UploadFile extends Component {
   }
 }
 
-
-
-
-//     render() {
-//         return (
-//             <div className='editions'>
-//                 <h1 className='text-center'>Lista Kontraktów</h1>
-
-//         <form>
-//         <label for="coordinators">Plik z koordynatorami umów:</label>
-//         <input id="coordinators" name="coordinators" type="file" />
-//         <label for="contracts">Plik z umowami:</label>
-//         <input id="contracts" name="contracts" type="file" />
-//         <label for="data">Plik z danymi z ankiet:</label>
-//         <input id="data" name="data" type="file" />
-//         <button type="submit" class="primary submit-btn">Submit</button>
-//       </form>
-//       </div>
-
-//         );
-//     }
-// }
-
 export default UploadFile;

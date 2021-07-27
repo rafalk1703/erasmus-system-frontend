cc9723adc30778ec4e2eff93d24ffc0df58082b7
Dawid Kulma <dawidkulma@gmail.com>
2021-07-27 19:08:23 +0200

    Make table looks like on mockup

--- src/components/QualificationTable.js
+++ src/components/QualificationTable.js
@@ -1,6 +1,6 @@
 import React from "react";
+import {Card, CardColumns, Row, Col, Button} from "react-bootstrap";
 import QualificationService from '../services/QualificationService';
-import {Table} from "react-bootstrap";
 import "./QualificationTable.css";
 
 class QualificationTable extends React.Component {
@@ -24,42 +24,54 @@ class QualificationTable extends React.Component {
         return (
             <div>
                 <h1 className='text-center'>Kwalifikacja Studentów</h1>
-                {
-                    this.state.contracts.map(
-                        contract =>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <div>
-                                            <h4 className='text-center'> {contract.erasmusCode} </h4>
-                                        </div>
-                                        <div>
-                                            {contract.contractsCoordinator.name} ilość miejsc: {contract.vacancies}
-                                        </div>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {
-                                        contract.registrations.map(
-                                            registration =>
-                                                <tr key={registration.id}>
-                                                    {registration.isAccepted}
-                                                    <div className='square'>
-                                                        {registration.priority}
-                                                    </div>
-                                                    <div>
-                                                        <h5>{registration.student.name} {registration.student.surname}</h5>
-                                                    </div>
-                                                    <div>
-                                                        {registration.student.department}, {registration.student.field}, {registration.student.year} rok
-                                                    </div>
-                                                </tr>
-                                        )
-                                    }
-                                </tbody>
-                            </table>
-                    )
-                }
+                <Row xs={1} md={3} className="g-4">
+                    {
+                        this.state.contracts.map(
+                            contract =>
+                                <Col>
+                                    <Card>
+                                        <Card.Body>
+                                            <Card.Title>
+                                                <h4 className='text-center'> {contract.erasmusCode} </h4>
+                                            </Card.Title>
+                                            <Card.Subtitle>
+                                                {contract.contractsCoordinator.name} ilość miejsc: {contract.vacancies}
+                                            </Card.Subtitle>
+                                            <CardColumns>
+                                                {
+                                                    contract.registrations.map(
+                                                        registration =>
+                                                            <Card key={registration.id}>
+                                                                <div>
+                                                                    <div>
+                                                                        <Button variant="secondary">+</Button>
+                                                                    </div>
+                                                                    <div className='square'
+                                                                         style={{
+                                                                             backgroundColor: (registration.priority == '1') ? '#FFD700' :
+                                                                                 (registration.priority == '2') ? '#C0C0C0' : '#A52A2A'
+                                                                         }}>
+                                                                        {registration.priority}
+                                                                    </div>
+                                                                    <div>
+                                                                        <div>
+                                                                            <h5>{registration.student.name} {registration.student.surname}</h5>
+                                                                        </div>
+                                                                        <div>
+                                                                            {registration.student.department}, {registration.student.field}, {registration.student.year} rok
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </Card>
+                                                    )
+                                                }
+                                            </CardColumns>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                        )
+                    }
+                </Row>
             </div>
         );
     }

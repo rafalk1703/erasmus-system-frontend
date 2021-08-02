7ab6e0683887e224266dcf274747b4d374a5ba1c
Dawid Kulma <dawidkulma@gmail.com>
2021-08-02 18:57:33 +0200

    Add qualification progress and conflicts marks

--- src/components/QualificationTable.js
+++ src/components/QualificationTable.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, CardColumns, Row, Col, Button, Container} from "react-bootstrap";
+import {Card, CardColumns, Row, Col, Button, Container, ProgressBar} from "react-bootstrap";
 import QualificationService from '../services/QualificationService';
 import "./QualificationTable.css";
 
@@ -8,19 +8,40 @@ class QualificationTable extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            contracts: []
+            contracts: [],
+            studentsRegistrations: {}
         }
     }
 
     componentDidMount() {
         QualificationService.getContracts().then(response => {
-            this.setState({contracts: response.data.body})
+            this.setState({contracts: response.data.contracts,
+                                studentsRegistrations: response.data.studentsRegistrations}
+            )
         }).catch(function (error) {
             console.log(error);
         });
     }
 
     render() {
+        const isConflict = (registration) => {
+            if (!registration.isAccepted) {
+                return false;
+            }
+            const studentsRegistrations = this.state.studentsRegistrations[registration.student.id];
+            let acceptedRegistrationsAmount = 0;
+            for (var i=0; i<studentsRegistrations.length; i++) {
+                this.state.contracts.map( contract => {
+                    contract.registrations.map( reg => {
+                        if (reg.id === studentsRegistrations[i] && reg.isAccepted) {
+                            acceptedRegistrationsAmount++;
+                        }
+                    })
+                })
+            }
+            return acceptedRegistrationsAmount > 1;
+        }
+
         return (
             <div>
                 <h1 id='header'>Kwalifikacja Studentów</h1>
@@ -45,12 +66,14 @@ class QualificationTable extends React.Component {
                                                     contract.registrations.map(
                                                         registration =>
                                                             <Card key={registration.id} id="reg-card"
-                                                                  style={{backgroundColor: registration.isAccepted ? '#d0f0c0' : '#FFFFFF'}}>
+                                                                  style={{
+                                                                      backgroundColor: isConflict(registration) ? '#ff9999' : registration.isAccepted ? '#d0f0c0' : '#FFFFFF'
+                                                                  }}>
                                                                 <div id="reg-div">
                                                                     <div className='square'
                                                                          style={{
-                                                                             backgroundColor: (registration.priority == 1) ? '#FFD700' :
-                                                                                 (registration.priority == 2) ? '#C0C0C0' : '#966919'
+                                                                             backgroundColor: (registration.priority === 1) ? '#FFD700' :
+                                                                                 (registration.priority === 2) ? '#C0C0C0' : '#966919'
                                                                          }}>
                                                                         <h5 id="square-priority">{registration.priority}</h5>
                                                                     </div>
@@ -83,6 +106,13 @@ class QualificationTable extends React.Component {
                                                     )
                                                 }
                                             </CardColumns>
+                                            <Card.Footer>
+                                                    <ProgressBar id="progress"
+                                                                 variant={contract.acceptedStudentsAmount < contract.vacancies ? "warning" : "success"}
+                                                                 now={contract.acceptedStudentsAmount / contract.vacancies * 100}
+                                                                 label={`${contract.acceptedStudentsAmount} z ${contract.vacancies}`}
+                                                    />
+                                            </Card.Footer>
                                         </Card>
                                     </Col>
                             )

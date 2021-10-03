779656dbe5df45437f26b7c2df0b5dd23869c6ee
Rafał Krajewski <rafalk1703@gmail.com>
2021-10-03 16:47:46 +0200

    more style improvement

--- src/components/EditionsList.js
+++ src/components/EditionsList.js
@@ -47,6 +47,24 @@ class EditionsList extends Component {
                 return <Alert variant="danger">nieaktywna</Alert>;
             }
         }
+
+        const renderDeactivateButton = (isActive, id) => {
+            if (isActive) {
+                return <Button onClick={() => this.deactiveEdition(id)} id="deactive" variant="outline-primary">Archiwizuj Edycję</Button>;
+            } else {
+                return "";
+            }
+        }
+
+        const renderEditButton = (isActive, id) => {
+            if (isActive) {
+                return <Link to={`/editEdition/${id}`}>
+                <Button id="details" variant="outline-primary">Edytuj Edycję</Button>
+            </Link>;
+            } else {
+                return "";
+            }
+        }
         return (
             <div>
                 <h1 className='text-center'>Lista edycji</h1>
@@ -55,6 +73,8 @@ class EditionsList extends Component {
                         <tr>
                             <td>Czy aktywna</td>
                             <td>Edycja</td>
+                            <td></td>
+                            <td>Listy</td>
                         </tr>
                     </thead>
                     <tbody>
@@ -65,19 +85,18 @@ class EditionsList extends Component {
                                         <td>{renderIsActive(editions.isActive)}</td>
                                         <td> {editions.year} </td>
                                         <td>
-                                            <Link to={`/editEdition/${editions.id}`}>
-                                                <Button id="details" variant="outline-primary">Edytuj Edycję</Button>
-                                            </Link>
+                                            {renderEditButton(editions.isActive, editions.id)}
                                             <Link to={`/editionStatistics/${editions.id}`}>
                                                 <Button id="details" variant="outline-primary">Szczegóły Edycji</Button>
                                             </Link>
 
                                             <Button onClick={() => this.deleteEdition(editions.id)} id="delete" variant="outline-primary">Usuń Edycję</Button>
-                                            <Button onClick={() => this.deactiveEdition(editions.id)} id="deactive" variant="outline-primary">Archiwizuj Edycję</Button>
+                                            {renderDeactivateButton(editions.isActive, editions.id)}
                                         </td>
-                                        <td>
-                                            <a download href={"http://localhost:8080/api/file/generate/" + editions.id} id="generate" variant="outline-primary">Pobierz Listę spoza WIEIT</a><br/>
-                                            <a download href={"http://localhost:8080/api/file/generate/WIEIT/" + editions.id} id="generate_wieit" variant="outline-primary">Pobierz Listę WIEIT</a>
+                                        <td><div class="btn-group-vertical">
+                                            <a download href={"http://localhost:8080/api/file/generate/" + editions.id} id="generate" class="btn btn-outline-primary" variant="outline-primary">Pobierz Listę spoza WIEIT</a>
+                                            <a download href={"http://localhost:8080/api/file/generate/WIEIT/" + editions.id} id="generate_wieit" class="btn btn-outline-primary" variant="outline-primary">Pobierz Listę WIEIT</a>
+                                            </div>
                                         </td>
                                     </tr>
                             )

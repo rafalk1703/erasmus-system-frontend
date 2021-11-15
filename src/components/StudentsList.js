8561947dcdcad95e941eea65b7c852d0c5f963f1
Dawid Kulma <dawidkulma@gmail.com>
2021-11-15 16:30:01 +0100

    Add filtering at students data view

--- src/components/StudentsList.js
+++ src/components/StudentsList.js
@@ -1,14 +1,19 @@
 import React, {Component} from "react";
 import {Accordion, Card} from "react-bootstrap";
+import {InputLabel} from "@material-ui/core";
+import Select from "react-select";
 import EditionService from "../services/EditionService";
 import StudentsService from "../services/StudentsService";
+import "./StudentsList.css"
 
 class StudentsList extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            students: []
+            all: [],
+            students: [],
+            names: ""
         };
     }
 
@@ -17,17 +22,57 @@ class StudentsList extends Component {
             (async () => {
                 await EditionService.getActiveEdition().then((response1) => {
                     StudentsService.getStudentsAllDataByEdition(response1.data.id).then((response) => {
-                        this.setState({ students: response.data } );
+                        this.setState({
+                            all: response.data,
+                            students: response.data
+                        });
                     });
                 });
             })()
         } catch (err) {}
     }
 
+    filterByNames = (entry) => {
+        let names = entry.value;
+        if (names === "") {
+            this.setState({
+                names: "",
+                students: this.state.all
+            });
+        } else {
+            const splitNames = names.split(" ");
+            this.setState({
+                names: names,
+                students: this.state.all.filter(
+                    student => {
+                        return student.name.indexOf(splitNames[0]) >=0 &&
+                            student.surname.indexOf(splitNames[1]) >=0
+                    }
+                )
+            });
+        }
+    }
+
     render() {
+
+        const namesOptions = this.state.all.map((student) => ({
+            value: student.name + " " + student.surname,
+            label: student.name + " " + student.surname
+        }));
+
+        namesOptions.push({
+            value: "",
+            label: "All"
+        });
+
         return (
             <div>
                 <h1 className="text-center">Dane studentów</h1>
+                <br></br>
+                <div id="select">
+                    <InputLabel id="label">Imię i nazwisko:</InputLabel>
+                    <Select options={namesOptions} onChange={this.filterByNames} placeholder="All"/>
+                </div>
                 <br></br>
                 <Accordion>
                     {
